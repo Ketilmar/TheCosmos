9378d92cd162163373f5e080676eadb80978abcd
kennethkodehode <kenneth.kodehode@gmail.com>
2022-10-11 09:13:05 +0200

    Added basic router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
 import GlobalStyles from './GlobalStyles';
+
+const Placeholder = () => (<div className="App"></div>)
 
 function App() {
   return (
     <>
       <GlobalStyles/>
-      <div className="App"></div>
+      {/* TODO: Add site header component here */}
+      <Routes>
+        <Route path="*" element={<Placeholder /> /* TODO: Replace with 404 page */} />
+        <Route path="/" element={<Placeholder /> /* TODO: Replace with landing page */} />
+        <Route path="/planets" element={<Placeholder /> /* TODO: Replace with planets page */} />
+        <Route path="/planet/:id" element={<Placeholder /> /* TODO: Replace with planet page */} />
+        <Route path="/about" element={<Placeholder /> /* TODO: Replace with about page */} />
+        <Route path="/contact" element={<Placeholder /> /* TODO: Replace with contact page */} />
+      </Routes>
+      {/* TODO: Add site footer component here */}
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
