124ae0375e9395e777605f6d5bee13c69f0ed5c5
kennethkodehode <kenneth.kodehode@gmail.com>
2022-10-11 12:58:10 +0200

    Added planets page

---
+++ src/pages/Planets.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+import { planets } from '../constants'
+
+import CelestialObjectCard from '../components/CelestialObjectCard'
+
+const Planets = () => (
+  <>
+    <h1>Planets</h1>
+    <StyledPlanetsSection>
+      {planets.map((planet) => (
+        <StyledCelestialObjectCard
+          color={planet.color}
+          name={planet.name}
+          to={`/body/${planet.id}`}
+        />
+      ))}
+    </StyledPlanetsSection>
+  </>
+)
+
+const StyledPlanetsSection = styled.section`
+  display: flex;
+  gap: 1em;
+  overflow-x: auto;
+
+  @media (min-width: 26em) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    overflow-x: initial;
+  }
+
+  @media (min-width: 48em) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 56em) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`
+
+const StyledCelestialObjectCard = styled(CelestialObjectCard)`
+  min-width: 14em;
+
+  @media (min-width: 26em) {
+    min-width: initial;
+  }
+`
+
+export default Planets
