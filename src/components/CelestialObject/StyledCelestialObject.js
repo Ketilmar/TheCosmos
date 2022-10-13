4156ae3505043b36b5f742e6649f7d44342c3b63
kennethkodehode <kenneth.kodehode@gmail.com>
2022-10-13 18:48:16 +0200

    Simplified CelestialObject component

--- src/components/CelestialObject/StyledCelestialObject.js
+++ src/components/CelestialObject/StyledCelestialObject.js
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
+const startColor = "#A7A7A7";
+const endColor   = "#0F131F";
+
 export const StyledCelestialObject = styled.div`
   width: 100%;
   height: 100%;
   aspect-ratio: 1;
   border-radius: 50%;
-  background: ${props => props.startColor};
+  background: ${props => props.color};
   background: linear-gradient(
     45deg,
-    ${props => props.endColor} 0%,
-    ${props => props.startColor} 100%
+    ${endColor} 0%,
+    ${props => props.color || startColor} 100%
   );
 `;
