19d569980e96c5a8d590f71d58afcbd374fe9327
ketilmar <ketilmar.kodehode@gmail.com>
2022-10-14 01:42:09 +0200

    basename as '/'

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="TheCosmos">
+    <HashRouter basename="/">
       <App />
     </HashRouter>
   </React.StrictMode>
