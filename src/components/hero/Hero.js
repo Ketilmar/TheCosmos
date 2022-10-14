92b13f844992cba30d822cff8201fcc0b53ce645
MarkusHodeKode <markusmj.hodekode@gmail.com>
2022-10-14 06:57:24 +0200

    Hero Structure Update

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,46 @@
+const Hero = () => (
+    <>
+        <StyledSection>
+            <article>
+                <h1>Get to know our neighbourhood better</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta, diam quis semper tincidunt, ante ante pellentesque lectus, vitae mollis tellus massa non risus. Nullam non eros auctor, viverra erat eget, condimentum nibh.</p>
+                <Button color="blue">Start Exploring</Button>
+            </article>
+        </StyledSection>
+
+        <StyledSection>
+            <h2>Knowledge right at your fingertips</h2>
+            <article>
+                <span>
+                    <img></img>
+                </span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </article>
+            <article>
+                <span>
+                    <img></img>
+                </span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </article>
+            <article>
+                <span>
+                    <img></img>
+                </span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </article>
+        </StyledSection>
+
+        <StyledSection>
+            <article>
+                <h3>Gather information about Mars and its two moons, Phobos and Deimos</h3>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+                <Button color="red">Explore Mars</Button>
+                <span>
+                    <img></img>
+                </span>
+            </article>
+        </StyledSection>
+    </>
+)
+
+export default Hero;
